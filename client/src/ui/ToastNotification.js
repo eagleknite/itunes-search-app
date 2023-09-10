84443d18f3cdc16e33155103dbaa1e16333ade63
Arnold Twala <arnoldtwl@gmail.com>
2023-09-10 14:19:56 +0200

    removed client folder

--- client/src/ui/ToastNotification.js
+++
@@ -1,9 +0,0 @@
-function ToastNotification({ message, type }) {
-    return (
-        <div className={`fixed top-4 right-4 bg-${type === 'error' ? 'red' : 'green'}-600 text-white rounded-md p-4 shadow-md`}>
-            {message}
-        </div>
-    );
-}
-
-export default ToastNotification;
